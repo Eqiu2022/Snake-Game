7a2fd102407893ad4f16d13fa8add686771cca45
Eqiu2022 <Eqiu2022@gmail.com>
2026-01-23 16:49:52 -0800

    create the box

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 #include <deque>
 #include <vector>
+
+using namespace std;
+
 int main(){
+    int width  = 30;
+    int height = 20;
+
+    //top
+    cout << '+';
+    for(int i = 0; i < width; i++){
+        cout << '-';
+    }
+    cout << '+' << endl;
+
+    //middle
+    for(int i = 0; i < height; i++){
+        cout << '|';
+        for(int j = 0; j < width; j++){
+            cout << endl;
+        }
+    }
+    //bottom
+    cout << '+';
+    for(int i = 0; i < width; i++){
+        cout << '-';
+    }
+    cout << '+' << endl;
 
 }
